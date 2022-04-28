27ce75ca58c724083f384cafeeb32bbf86db8ec3
wei <leo2255leo2255@gmail.com>
2022-04-28 15:47:22 +0800

    set third party sign in

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # 新增此段第三方登入的方法
+  def google_oauth2
+    @user = User.create_from_provider_data(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+  def github
+    @user = User.create_from_provider_data(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Github"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.github_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+  # 及這段第三方登入失敗的處理方法
+  def failure
+    redirect_to root_path
+  end
+end
