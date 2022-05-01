fbea83995a8766ee8d7c2dc8f6170b6f4f780dcc
Kris <krischiang0301@gmail.com>
2022-05-02 00:09:01 +0800

    add comment model / comment

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ApplicationRecord
+  has_many :comments
   has_rich_text :content
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users , controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :message_boards, only: [:index], as: "message_boards"
-  resources :messages
+  resources :messages do
+    resources :comments
+  end
   
   root "home#index"
 end
