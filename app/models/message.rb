669e74a54e5e5eb3c5497bd0ceb869df56cef4ad
Kris <krischiang0301@gmail.com>
2022-05-02 14:01:04 +0800

    add commemt destroy

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   include Visible
   
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_rich_text :content
 
   validates :title, presence: true
