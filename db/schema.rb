4c3e4a480cdb4f8ab0ff6168e4a6767cd99ca14f
Easyfun0 <s5300012@hotmail.com>
2022-05-02 22:46:37 +0800

    新增行事曆 (#48)
    1.新增行事曆
    2.新增行程,刪除,修改

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2022_04_28_122027) do
 ActiveRecord::Schema.define(version: 2022_04_28_050123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "schedules", force: :cascade do |t|
+    t.string "title"
+    t.integer "calendar_id"
+    t.datetime "start"
+    t.datetime "end"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+  
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
