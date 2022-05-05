8ceb62de876eefcd332e1cac9ba02da5366b0a80
AshΞAnn <50226428+AsheAnn@users.noreply.github.com>
2022-05-05 12:07:39 +0800

    Hotfix (#52)
    * message_boards CRUD
    
    * add active storage / active text / Trix
    
    * add comment model / comment
    
    * add message/comment status
    
    * add commemt destroy
    
    * add style
    
    * fix style css / new message btn
    
    * fix: install lost node module
    
    fixed date.order i18n value and yarn add tui-calendar
    
    Co-authored-by: Kris <krischiang0301@gmail.com>
    Co-authored-by: Easyfun0 <s5300012@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,9 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2022_05_02_051147) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,7 +78,7 @@ ActiveRecord::Schema.define(version: 2022_05_02_051147) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
