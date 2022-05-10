04d2fbefb2f1a954df0295489c548b3d24141f47
wei <leo2255leo2255@gmail.com>
2022-05-10 20:21:08 +0800

    before add join member feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
-    rescue_from NoMethodError, with: :not_found
-
-    include Pundit::Authorization
     
+    include Pundit::Authorization
+
     def not_found 
-        render status:404
+        render file:"#{Rails.root}/public/404.html",
+        status: 404,
+        layout: false
     end
 
     def after_sign_in_path_for(resource)
--- app/controllers/personals_controller.rb
+++ app/controllers/personals_controller.rb
@@ -3,7 +3,7 @@ class PersonalsController < ApplicationController
     before_action :authenticate_user! 
     before_action :findmyproject , only: %i[show edit update destroy]
     def index
-        if user_signed_in?
+        if user_signed_in? 
             @projects = current_user.projects.all 
         else 
             redirect_to new_user_session_path , notice:"請登入！"
