6a1003d78df68a30721b0528634fd4d971409bb4
Easyfun0 <s5300012@hotmail.com>
2022-05-12 15:25:14 +0800

    新增TodoList
    1.新增TodoList CRUD
    2.新增TodoItem CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 
 
+
 ActiveRecord::Schema.define(version: 2022_05_13_064651) do
 
 
@@ -114,6 +115,22 @@ ActiveRecord::Schema.define(version: 2022_05_13_064651) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "todo_items", force: :cascade do |t|
+    t.text "description", null: false
+    t.boolean "completed"
+    t.datetime "completed_at"
+    t.bigint "todo_list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -143,4 +160,5 @@ ActiveRecord::Schema.define(version: 2022_05_13_064651) do
   add_foreign_key "personals", "projects"
   add_foreign_key "personals", "users"
   add_foreign_key "projects", "users"
+  add_foreign_key "todo_items", "todo_lists"
 end
