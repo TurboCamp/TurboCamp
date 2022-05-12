4d0f11240183ea80c4d2757dcd80f38649a40cda
Easyfun0 <s5300012@hotmail.com>
2022-05-12 10:58:50 +0800

    新增Nested_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :todo_lists do
+    resources :todo_items
+  end
+  namespace :todo_list do
+    resources :todo_items
+  end
   resources :schedules
   devise_for :users , controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
