3b3f70492cee54576fb912dbbd686da0179df3e5
Easyfun0 <s5300012@hotmail.com>
2022-05-12 10:58:50 +0800

    新增Nested_form

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   match "/404", to: "application#not_found", via: :all
 
 
-
   resources :schedules
   devise_for :users , controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
