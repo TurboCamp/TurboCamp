93d7214f2f9eef654dd5f7711880c4cf4ee234b6
wei <leo2255leo2255@gmail.com>
2022-05-17 20:24:50 +0800

    set tailwind reset

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "stylesheets/application";
 import LocalTime from "local-time"
 LocalTime.start()
 
