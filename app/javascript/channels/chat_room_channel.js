a2ec8db04735a7e751592cd7b7fcb18466cdd286
wei <leo2255leo2255@gmail.com>
2022-05-18 16:54:54 +0800

    fix

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -5,24 +5,16 @@ let room_id = parseInt(url.substring(url.search("chat_rooms/") + 11))
 if (url.indexOf("chat_rooms/") != -1 ){
   consumer.subscriptions.create({"channel":"ChatRoomChannel", "chat_room_id":room_id},{
     connected() {
-      console.log(`connect to ${room_id}`)
     },
-
     disconnected() {
       // Called when the subscription has been terminated by the server
     },
-
     received(data) {
-      console.log(data);
-      
-
-      const ii = `<div class="w-max ml-auto">
+      const message = `<div class="w-max ml-auto">
       <span class="text-xs block">${data.user}</span>
       <div class=" px-3 py-2 bg-gray-200 opacity-3 rounded-lg w-max">
-      ${data.text} 
-  </div>
-  </div>`
-      document.querySelector("#content").insertAdjacentHTML("beforeend" , ii)
+      ${data.text} </div></div>`
+      document.querySelector("#content").insertAdjacentHTML("beforeend" , message)
 
     }
   }
