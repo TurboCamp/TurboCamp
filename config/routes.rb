cb105931877acc39928f73562c98a7b8307e9c89
wei <leo2255leo2255@gmail.com>
2022-05-18 18:22:30 +0800

    reset routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,6 @@
 
 Rails.application.routes.draw do
   match '/404', to: 'application#not_found', via: :all
-
-  resources :todo_lists do
-    resources :todo_items
-  end
-
-  namespace :todo_list do
-    resources :todo_items
-  end
   
   devise_for :users , controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
@@ -18,22 +10,7 @@ Rails.application.routes.draw do
     post 'users/info' , to:'users/infos#setting'
   end 
 
-  resources :schedules
-  resources :messages do
-    resources :comments, module: :messages
-  end
-
-  resources :buckets do
-    resources :comments, module: :buckets
-    collection do
-      get 'document'
-      get 'upload'
-    end
-  end
   root 'home#index'
-
-  resources :chat_rooms, only: [:show]
-  resource :contents, only: [:create]
 
   namespace :api do
     namespace :v1 do
@@ -46,5 +23,24 @@ Rails.application.routes.draw do
   end
 
   resources :personals, only: [:index]
-  resources :projects, expect: [:index]
+
+  resources :projects, expect: [:index] do 
+    resources :buckets do
+      resources :comments, module: :buckets
+      collection do
+        get 'document'
+        get 'upload'
+      end
+    end
+    resources :chat_rooms, only: [:show]
+    resource :contents, only: [:create]
+    resources :schedules
+    resources :messages do
+      resources :comments, module: :messages
+    end
+    
+    resources :todo_lists do
+      resources :todo_items
+    end
+  end
 end
