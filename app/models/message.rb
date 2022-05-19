5c3cde2a91a064781933a330070e42d1a918e109
Kris <krischiang0301@gmail.com>
2022-05-19 18:08:29 +0800

    style/messages->index

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,7 +5,7 @@ class Message < ApplicationRecord
 
   has_rich_text :content
   has_many :comments, as: :commentable, dependent: :destroy
-  belongs_to :project
+  # belongs_to :project
 
   validates :title, presence: true
 end
