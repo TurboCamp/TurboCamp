b65930d204e6bd2feda2c6ea88da7042436e672b
wei <leo2255leo2255@gmail.com>
2022-05-20 15:34:49 +0800

    message can edit but delete can not redirect

--- app/controllers/personal_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class PersonalController < ApplicationController
-  before_action :authenticate_user!
-  before_action :findmyproject, only: %i[show edit update destroy]
-  def index
-      @projects = current_user.projects
-  end
-end
