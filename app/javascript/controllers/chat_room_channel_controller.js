b15bcc9737095e0c5bc461461f4db97680276940
wei <leo2255leo2255@gmail.com>
2022-05-23 17:16:08 +0800

    聊天室暫定樣子

--- app/javascript/controllers/chat_room_channel_controller.js
+++ app/javascript/controllers/chat_room_channel_controller.js
@@ -9,23 +9,22 @@ export default class extends Controller {
     if (this.contentboxTarget) {
       let chat_room_id = this.element.dataset.id
       consumer.subscriptions.create(
-        { "channel": "ChatRoomChannel", "chat_room_id": chat_room_id },
-        { 
+        { "channel": "ChatRoomChannel", "chat_room_id": chat_room_id },{ 
           received(data) {
-           const image = `<div class="w-max mt-5">
-           <span class="text-xs block">${data.send_by}</span>
-           <div class=" px-3 py-2 bg-gray-200 opacity-3 rounded-lg w-max">
-           ${data.message}<a href="${data.image}"><img src="${data.image}" width="150" height="150"></a></div></div>`
-           
-          const message = `<div class="w-max mt-5">
-          <span class="text-xs block">${data.send_by}</span>
-          <div class=" px-3 py-2 bg-gray-200 opacity-3 rounded-lg w-max">
-          ${data.message}</div></div>`
-          if (data.image == null){
-            document.querySelector("#content").insertAdjacentHTML("beforeend" , message)
-          }else{
-            document.querySelector("#content").insertAdjacentHTML("beforeend" , image)
-          }
+            const image = `<div class="w-max mt-5">
+            <span class="text-xs block">${data.send_by}</span>
+            <div class=" px-3 py-2 bg-gray-200 opacity-3 rounded-lg w-max">
+            ${data.message}<a href="${data.image}"><img src="${data.image}" width="150" height="150"></a></div></div>`
+            
+            const message = `<div class="w-max mt-5">
+            <span class="text-xs block">${data.send_by}</span>
+            <div class=" px-3 py-2 bg-gray-200 opacity-3 rounded-lg w-max">
+            ${data.message}</div></div>`
+            if (data.image == null){
+              document.querySelector("#content").insertAdjacentHTML("beforeend" , message)
+            }else{
+              document.querySelector("#content").insertAdjacentHTML("beforeend" , image)
+            }
           },
         }
       )
