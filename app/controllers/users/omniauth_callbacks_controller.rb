42c3af93d8f60b2ab37c329fd266d15064b54d0f
Easyfun0 <s5300012@hotmail.com>
2022-05-23 19:44:49 +0800

    Fix heroku error7

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,7 +11,7 @@ module Users
         sign_in_and_redirect @user, event: :authentication
       else
         session['devise.google_data'] = request.env['omniauth.auth']
-        redirect_to new_user_registration_url
+        redirect_to new_user_registration_path
       end
     end
 
@@ -23,7 +23,7 @@ module Users
         sign_in_and_redirect @user, event: :authentication
       else
         session['devise.github_data'] = request.env['omniauth.auth']
-        redirect_to new_user_registration_url
+        redirect_to new_user_registration_path
       end
     end
 
