cfe78ac51279ec769e11ba9b6249a31b55cb9092
Kris <krischiang0301@gmail.com>
2022-05-24 16:19:09 +0800

    fix conflict

---
+++ app/uploaders/image_uploader 2.rb
@@ -0,0 +1,14 @@
+require "image_processing/mini_magick"
+
+class ImageUploader < Shrine 
+  Attacher.derivatives do |original|
+    magick = ImageProcessing::MiniMagick.source(original)
+ 
+    { 
+      large:  magick.resize_to_limit!(800, 800),
+      medium: magick.resize_to_limit!(150, 150),
+      small:  magick.resize_to_limit!(30, 30),
+    }
+    validate_mime_type %w[image/jpeg image/png image/webp]
+  end
+end
