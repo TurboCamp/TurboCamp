66c952568916473306cad2c666d379e7e9d4e424
wei <leo2255leo2255@gmail.com>
2022-05-25 17:52:40 +0800

    可上傳可下載

--- app/javascript/controllers/upload_file_controller.js
+++ app/javascript/controllers/upload_file_controller.js
@@ -1,8 +1,17 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-
+  static targets = ['upload_form']
   connect(){
     console.log(123)
+  }
+
+  upload(e) {
+    e.preventDefault()
+    this.upload_formTarget.classList.remove("hidden")
+  }
+
+  submit(){
+    this.upload_formTarget.classList.add("hidden")
   }
 }
--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -14,9 +14,9 @@ s3 = Shrine.storages = {
   store: Shrine::Storage::S3.new(prefix: "store",**s3_options),
 }
 
-Shrine.plugin :activerecord           # loads Active Record integration
-Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
-Shrine.plugin :restore_cached_data    # extracts metadata for assigned cached files
+Shrine.plugin :activerecord
+Shrine.plugin :cached_attachment_data 
+Shrine.plugin :restore_cached_data    
 Shrine.plugin :validation
 Shrine.plugin :validation_helpers
 Shrine.plugin :download_endpoint , prefix: "/attachments"
