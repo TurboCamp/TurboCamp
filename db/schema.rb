0477016b75ae6378c0371ac00fbd1d5099d2ff85
Rex Chen <rexchen628@gmail.com>
2022-05-26 14:24:25 +0800

    removed comments in user.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_05_25_071433) do
 
   # These are extensions that must be enabled in order to support this database
@@ -119,6 +118,13 @@ ActiveRecord::Schema.define(version: 2022_05_25_071433) do
     t.bigint "project_id"
     t.string "slug"
     t.index ["project_id"], name: "index_messages_on_project_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "slug"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "personals", force: :cascade do |t|
