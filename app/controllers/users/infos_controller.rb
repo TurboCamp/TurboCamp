906aeaf127607cacbaaa3a2f4d358301f3b656cb
ashe <asiasheann@gmail.com>
2022-05-26 19:16:36 +0800

    adjust user info setting

--- app/controllers/users/infos_controller.rb
+++ app/controllers/users/infos_controller.rb
@@ -1,22 +1,24 @@
-class Users::InfosController < ApplicationController 
-    before_action :authenticate_user!
-    def info 
-        if user_signed_in?
-            @user = current_user
-        else 
-            redirect_to new_user_session_path
-        end
-    end
-    def setting
-        @user = current_user
-        if @user.update(update_user_info)
-            redirect_to  personal_path
+class Users::InfosController < ApplicationController
+  before_action :authenticate_user!
+  layout 'infos'
 
-        end 
+  def info
+    
+    if user_signed_in?
+      @user = current_user
+    else
+      redirect_to new_user_session_path
     end
-    private 
+  end
 
-    def update_user_info
-        params.require(:user).permit(:email , :imageurl , :nickname , :avatar , :slug)
-    end
+  def setting
+    @user = current_user
+    redirect_to personal_path if @user.update(update_user_info)
+  end
+
+  private
+
+  def update_user_info
+    params.require(:user).permit(:email, :imageurl, :nickname, :avatar, :slug)
+  end
 end
--- app/javascript/controllers/modal_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@hotwired/stimulus'
-
-export default class extends Controller {
-  static targets = ['container']
-
-  close() {
-    this.element.setAttribute("class", "hidden")
-  }
-}
