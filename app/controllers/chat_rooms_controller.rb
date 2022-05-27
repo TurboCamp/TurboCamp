9cae829867bfc82209261a6628a4ec7326b6a90a
ashe <asiasheann@gmail.com>
2022-05-27 18:08:07 +0800

    adjust chatroom layout style

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -2,11 +2,12 @@
 
 class ChatRoomsController < ApplicationController
   before_action :authenticate_user!
+  layout 'chatroom'
 
   def show
-      @project = current_user.projects.friendly.find(params[:project_id])
-      @chat_room = @project.chat_room
-      @contents = @chat_room.contents
-      @content = Content.new
+    @project = current_user.projects.friendly.find(params[:project_id])
+    @chat_room = @project.chat_room
+    @contents = @chat_room.contents
+    @content = Content.new
   end
 end
