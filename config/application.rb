d5c778632e9e6d7e10822820985029fb5f082de2
wei <leo2255leo2255@gmail.com>
2022-05-28 18:02:34 +0800

    聊天室增加時間

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module TurboCamp
     # in config/environments, which are processed later.
 
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Taipei"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
