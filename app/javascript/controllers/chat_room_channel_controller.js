4acf34042eb6973da6afa50c95fce821de6329e6
wei <leo2255leo2255@gmail.com>
2022-06-01 21:20:35 +0800

    讓聊天室能自動滑動

--- app/javascript/controllers/chat_room_channel_controller.js
+++ app/javascript/controllers/chat_room_channel_controller.js
@@ -6,14 +6,15 @@ export default class extends Controller {
   static targets = ['contentbox' , 'textarea' , 'file_upload' , 'message_input' , 'submit_btn']
   initialize() {
     const content = document.querySelector("#content")
-    content.scrollTo(0 , content.scrollHeight)
     if (this.hasContentboxTarget) {
       const chat_room_id = this.element.dataset.roomId 
       const private_chat_id = this.element.dataset.privateId
-
+      
       createConsumer().subscriptions.create(
         { "channel": "ChatRoomChannel", chatRoomId: chat_room_id  , privateChatId: private_chat_id},{ 
           connected(){
+            const content = document.querySelector("#content")
+            content.scrollTo(0 , content.scrollHeight)
             console.log(`connect: private-${private_chat_id}`);
             console.log(`connect: chat-${chat_room_id }`);
             
@@ -24,7 +25,7 @@ export default class extends Controller {
             document.querySelector("#content_text").value = ""
             document.querySelector("input[type='submit']").disabled = false
             document.querySelector("#content_image").value = ''          
-            content.scrollTo(0 , content.scrollHeight)
+            content.scrollTop = content.scrollHeight
           },
         }
       )
