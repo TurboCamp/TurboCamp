cc2dd47beef7d96089b9a1cee303b62d64809545
Easyfun0 <s5300012@hotmail.com>
2022-06-03 00:00:39 +0800

    修改status
    1.畫面修改
    2.新增預設值todos

--- db/migrate/20220530133211_add_status_to_todolists.rb
+++ db/migrate/20220530133211_add_status_to_todolists.rb
@@ -1,6 +1,6 @@
 class AddStatusToTodolists < ActiveRecord::Migration[6.1]
   def change
-    add_column :todo_lists, :status, :string
+    add_column :todo_lists, :status, :string, default: 'todos'
     add_column :todo_lists, :position, :integer
     add_column :todo_lists, :start, :datetime
   end
