758cb26e89dba1ba4d5cd301d788ae99c87200be
Easyfun0 <s5300012@hotmail.com>
2022-06-02 16:22:08 +0800

    修改todolist css

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -1,36 +1,16 @@
 import { Controller } from '@hotwired/stimulus'
 import Sortable from 'sortablejs'
 import Rails from '@rails/ujs'
-// import Sortable from 'stimulus-sortable'
 
 export default class extends Controller {
   static values = {
     status: String
   }
   connect() {
-    console.log(this.statusValue);
-    
     Sortable.create(this.element, {
       group: 'shared',
-      // onEnd: this.end.bind(this),
       onChange: this.end.bind(this),
-      // onMove: function (/**Event*/evt, /**Event*/originalEvent) {
-
-      //   // console.log(evt);
-      //   evt.dragged; // dragged HTMLElement
-      //   evt.draggedRect; // DOMRect {left, top, right, bottom}
-      //   evt.related; // HTMLElement on which have guided
-      //   evt.relatedRect; // DOMRect
-      //   evt.willInsertAfter; // Boolean that is true if Sortable will insert drag element after target by default
-      //   originalEvent.clientY; // mouse position
-      //   // return false; — for cancel
-      //   // return -1; — insert before target
-      //   // return 1; — insert after target
-      //   // return true; — keep default insertion point based on the direction
-      //   // return void; — keep default insertion point based on the direction
-      // },
     })
-    
   }
   end(event) {
 
@@ -42,8 +22,7 @@ export default class extends Controller {
     } else if (this.statusValue === 'c') {
       status = 'finish'
     }
-    console.log(this.statusValue);
-    console.log(status);
+
     const data = new FormData()
 
     data.append("position", event.newIndex + 1)
@@ -52,13 +31,7 @@ export default class extends Controller {
     Rails.ajax({
       url: `./todo_lists/${id}/move`,
       type: 'PATCH',
-      data: data,
-      success: (resp)=>{
-        console.log(resp);
-      },
-      error: (err)=>{
-          console.log(err);
-      }
+      data: data
     })
   }
 }
