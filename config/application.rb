548821c65608cd529af509f648c75bdc4a4935cd
ashe <asiasheann@gmail.com>
2022-06-03 19:19:08 +0800

    custom flash message style with modern notification

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module TurboCamp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.i18n.default_locale = 'zh-TW'
+    config.i18n.default_locale = :en
     config.autoloader = :classic
     # Configuration for the application, engines, and railties goes here.
     #
