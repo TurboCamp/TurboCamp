5cfb094cf66ebca49b27d091110ddf034fc559d9
wei <leo2255leo2255@gmail.com>
2022-06-05 23:40:19 +0800

    修復有時候未讀數量無法出現

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -10,16 +10,22 @@ class ContentsController < ApplicationController
       @content = @chat_room.contents.new(content_params)
       chat_room_id = params[:private_chat_id]
       private_or_not = true
+ 
     else
       @chat_room = @project.chat_room
       @content = @chat_room.contents.new(content_params)
       chat_room_id = @chat_room.id
       private_or_not = false
     end
-
+    
     if @content.save
       SendMessageJob.perform_later(@content, chat_room_id, private_or_not)
-      # render html: @content
+      #更新他人的未讀量，因為其他人沒進房間不會執行程式碼，所以須更新他人而不是自己
+      @project.private_chats.each do |room|
+        user_id = (room.roomname.split('Room:').last.split('-') - ["#{current_user.id}"]).first
+        unread_count = room.contents.where("created_at > ? AND user_id != ?", room.user_privates.find_by(user_id: user_id).enter_at , user_id).count
+        User.find(user_id).user_privates.where(private_chat_id:room.id).update(unread_count:unread_count)
+      end
     end
   end
 
