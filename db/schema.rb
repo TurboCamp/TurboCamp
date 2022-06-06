ab588f6c972155785c913d3a3923ab8fcf618c09
wei <leo2255leo2255@gmail.com>
2022-06-07 01:49:34 +0800

    修復第一次加入專案進入時間會是nil無法顯示未讀訊息

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_164212) do
+ActiveRecord::Schema.define(version: 2022_06_06_174541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2022_06_06_164212) do
   create_table "chat_users", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "chat_room_id", null: false
-    t.datetime "enter_at"
+    t.datetime "enter_at", default: "2022-06-06 17:47:10"
     t.integer "unread_count", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -206,7 +206,7 @@ ActiveRecord::Schema.define(version: 2022_06_06_164212) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "unread_count", default: 0
-    t.datetime "enter_at"
+    t.datetime "enter_at", default: "2022-06-06 17:47:10"
     t.index ["private_chat_id"], name: "index_user_privates_on_private_chat_id"
     t.index ["user_id"], name: "index_user_privates_on_user_id"
   end
