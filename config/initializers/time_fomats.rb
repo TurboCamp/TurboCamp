4420e053ac8151b28832d91e40cf723d10c831f6
wei <leo2255leo2255@gmail.com>
2022-06-09 17:38:58 +0800

    fix heroku2

--- config/initializers/time_fomats.rb
+++ config/initializers/time_fomats.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Time::DATE_FORMATS[:long_zh_tw] = '%Y年%m月%d日%H時%M分'
-Time::DATE_FORMATS[:long_no_separator] = '%m/%d/ %H:%M'
+Time::DATE_FORMATS[:long_no_separator] = '%m/%d %H:%M'
 Time::DATE_FORMATS[:hm_on_today] = '%H:%M'
 Time::DATE_FORMATS[:md_on_today_zh_tw] = '%m月%d日'
 Time::DATE_FORMATS[:message_at] = '%H：%M'
