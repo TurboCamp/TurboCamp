260dbf005c8f06304ca9ba5ed75721104263837e
wei <leo2255leo2255@gmail.com>
2022-06-09 11:19:11 +0800

    修改行事曆不可顯示顏色bug

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -1,30 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 import Cal from 'tui-calendar';
 import Rails from '@rails/ujs'
+import TuiDatePicker from 'tui-date-picker'
+import TuiCodeSnippet from 'tui-code-snippet'
 
-const statusColor = {
-  'My Calendar': {
-    id: '1',
-    color: '#ffffff',
-    bgColor: '#9e5fff',
-    dragBgColor: '#000',
-    borderColor: '#9e5fff',
-  },
-  'Todos': {
-    id: '2',
-    color: '#ffffff',
-    bgColor: '#00a9ff',
-    dragBgColor: '#000',
-    borderColor: '#00a9ff'
-  },
-  'Finish': {
-    id: '3',
-    color: '#ffffff',
-    bgColor: '#00a',
-    dragBgColor: '#000',
-    borderColor: '#00a'
-  }
-}
 const calendars =  [
   {
     id: '1',
@@ -61,78 +40,45 @@ const calendars =  [
 ]
 export default class extends Controller {
   
-
   getCalendarData() {
     let url = './schedules.json'
-    // let calendarsMap = new Map(Object.entries(calendars))
-    // console.log(calendarsMap);
-    // console.log(calendarsMap.get('name'))
-    
     fetch(url)
       .then(response => response.json())
       .then(response => response.forEach(schedule => {
+        console.log(schedule);
+        
         this.calendar.createSchedules([
           {
             id: schedule.id,
             title: schedule.title,
             location: schedule.location,
+            calendarId: schedule.calendar_id,
             category: 'time',
             start: schedule.start,
             dueDateClass: schedule.dataDataClass,
             end: schedule.end,
-            color: statusColor[schedule.name].color,
-            bgColor: statusColor[schedule.name].bgColor,
-            dragBgColor: statusColor[schedule.name].dragBgColor,
-            borderColor: statusColor[schedule.name].borderColor,
-          }
-        ])
-        // console.log(schedule.name);
-        function status(e) {
-          if (e.name === schedule.name) {
-            return e.name
-          }
-        }
-        console.log(calendars.find(status));
-
-      }))
-  }
-
+          }])}))    
+      }
   // 在日曆上新增行程
   createCalendarSchedule() {
     let calendar = this.calendar
     calendar.on('beforeCreateSchedule', (e) => {
-      // console.log(e);
-      // let newcalendars = calendars.filter(function (item) {
-      //   return item.name == e.name
-      // })
       let schedule = {
         id: 1,
-        calendarId: '1',
+        calendarId: e.calendarId,
         title: e.title,
         category: 'time',
         location: e.location,
         start: e.start,
         end: e.end,
-        // name: newcalendars.name
-        // name: calendars.find(c => c.name == e.name),
-        // color: statusColor[schedule.name].color,
-        // bgColor: statusColor[schedule.name].bgColor,
-        // dragBgColor: statusColor[schedule.name].dragBgColor,
-        // borderColor: statusColor[schedule.name].borderColor,
       }
-      // console.log(Object.entries(calendars));
       calendar.createSchedules([schedule])
       let formData = new FormData()
-      
-      console.log(schedule.name);
       formData.append('[schedule]title', schedule.title)
       formData.append('[schedule]start', schedule.start._date)
       formData.append('[schedule]end', schedule.end._date)
       formData.append('[schedule]location', schedule.location)
-      formData.append('[schedule]name', schedule.name)
-
-      console.log(formData);
-      console.log(schedule);
+      formData.append('[schedule]calendar_id', schedule.calendarId)
 
       Rails.ajax({
         type: 'post',
@@ -161,23 +107,17 @@ export default class extends Controller {
       if (changes.location) {
         formUpdate.append('[schedule]location', changes.location)
       }
+      if (changes.calendarId) {
+        formUpdate.append('[schedule]calendar_id', changes.calendarId)
+      }
       calendar.updateSchedule(schedule.id, schedule.calendarId, changes, {
         start: e.startTime,
         end: e.endTime,
         title: e.title,
         location: e.location,
+        calendar_id: e.calendarId
     });
       
-      // calendar.updateSchedule([schedule])
-      // let formData = new FormData()
-      
-      // formData.append('title', schedule.title)
-      // formData.append('start', schedule.start._date)
-      // formData.append('end', schedule.end._date)
-      // formData.append('location', schedule.location)
-      
-
-
       Rails.ajax({
         type: 'patch',
         url: './schedules/' + schedule.id,
@@ -200,13 +140,7 @@ export default class extends Controller {
     })
   }
 
-  
-
-
   connect() {
-
-
-    
     this.calendar = new Cal(document.getElementById('calendar'), {
       id: "1",
       name: "schedule",
@@ -234,21 +168,34 @@ export default class extends Controller {
           },
             milestone: function(schedule) {
                 return '<span style="color:red;"><i class="fa fa-flag"></i> ' + schedule.title + '</span>';
-            },
-          //   milestoneTitle: function() {
-          //       return 'Milestone';
-          //   },
-          //   task: function(schedule) {
-          //       return '&nbsp;&nbsp;#' + schedule.title;
-          //   },
-          //   taskTitle: function() {
-          //       return '<label><input type="checkbox" />Task</label>';
-          // },    
-        }
+            },  
+        }  
     })
 
-
-
+    this.calendar.setCalendarColor('1', {
+      color: '#ffffff',
+      bgColor: '#9e5fff',
+      dragBgColor: '#000',
+      borderColor: '#9e5fff',
+    })
+    this.calendar.setCalendarColor('2', {
+      color: '#ffffff',  
+      bgColor: '#00a9ff',
+      dragBgColor: '#00a9ff',
+      borderColor: '#00a9ff',
+    })
+    this.calendar.setCalendarColor('3', {
+      color: '#ffffff',
+      bgColor: '#fa0',
+      dragBgColor: '#fa0',
+      borderColor: '#fa0',
+    })
+    this.calendar.setCalendarColor('4', {
+      color: '#ffffff',
+      bgColor: '#00a',
+      dragBgColor: '#00a',
+      borderColor: '#00a',
+    })
     this.getCalendarData()
     this.createCalendarSchedule()
     this.updatedCalendarSchedule()
@@ -260,8 +207,5 @@ export default class extends Controller {
       }
     });
   }
-
-
-
 }
 
